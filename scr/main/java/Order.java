0910ef793fc9fe489e4dce485bb62cac31116125
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-10-01 07:14:38 +0300

    Commit DZ34 2023-1001 07:17

--- scr/main/java/Order.java
+++ scr/main/java/Order.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @Table(name = "o_order")
@@ -17,12 +18,16 @@ public class Order {
     @Column(nullable = false)
     private Double cost;
 
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<Product> products;
+
     public Order() {
     }
 
-    public Order(Timestamp date, Double cost) {
+    public Order(Timestamp date, Double cost, List<Product> products) {
         this.date = date;
         this.cost = cost;
+        this.products = products;
     }
 
     public Long getId() {
@@ -47,5 +52,13 @@ public class Order {
 
     public void setCost(Double cost) {
         this.cost = cost;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 }
